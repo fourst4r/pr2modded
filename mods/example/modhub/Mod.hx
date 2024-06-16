4defaf713105b836101d0bca8fe2ddee0032c7c5
fourst4r <oxygenmagnet@hotmail.com>
2024-06-16 21:14:57 +1200

    more clean

--- mods/example/modhub/Mod.hx
+++ mods/example/modhub/Mod.hx
@@ -1,27 +1,15 @@
 package modhub;
 
 import api.Game;
-import fl.controls.Button;
-import flash.errors.IOError;
 import flash.net.URLRequestHeader;
-import flash.events.SecurityErrorEvent;
 import haxe.Json;
 import flash.net.URLLoader;
-import package_18.LoadoutsPopup;
 import haxe.io.Path;
-// import api.*;
 import modding.*;
-import flash.events.UncaughtErrorEvent;
-import flash.system.ApplicationDomain;
-import flash.system.LoaderContext;
-import flash.display.LoaderInfo;
 import flash.events.IOErrorEvent;
 import flash.events.Event;
 import flash.net.URLRequest;
-import flash.display.Loader;
 import package_4.MessagePopup;
-import flash.system.Security;
-import flash.Lib;
 
 final class Mod extends modding.ModBase {
     var _modsLoaded:Array<ModLoader>;
@@ -29,9 +17,7 @@ final class Mod extends modding.ModBase {
     public override function init(ctx:ModContext) {
         super.init(ctx);
         _modsLoaded = [];
-        // final mods = ctx.allMods.copy();
-        // mods.remove(ctx.current); // exclude modhub
-        // new modhub.ui.ModMenu2(cast mods);
+        
         loadModList();
         
         // Game.pageHolder.getCurrentPage().addChild(new Button());
@@ -87,40 +73,10 @@ final class Mod extends modding.ModBase {
 
     public override function enable() {
         trace("modhub starting");
-        // loadMod("file:///C:/Users/oxyge/Documents/pr2dec/mods/example/bin/skinning.swf");
     }
 
     public override function disable() {
     }
-
-    // function loadMod(info:ModInfo, allMods:Array<ModInfo>) {
-    //     trace("Loading mod "+info.name);
-    //     final loader = new Loader();
-    //     loader.contentLoaderInfo.addEventListener(Event.COMPLETE, (e) -> {
-    //         trace("Loaded mod successfully "+info.name);
-    //         final loaderInfo:LoaderInfo = e.target;
-    //         Security.allowDomain(loaderInfo.loaderURL);
-    //         Security.allowInsecureDomain(loaderInfo.loaderURL);
-    //         Security.allowDomain(loaderInfo.url);
-    //         Security.allowInsecureDomain(loaderInfo.url);
-    //         loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, (e) -> {
-    //             new MessagePopup("inmodhub ERROR FFS: "+e);
-    //         });
-    //         Lib.current.stage.addChild(loaderInfo.content);
-            
-    //         final mod:IMod = untyped __new__(__as__(loaderInfo.applicationDomain.getDefinition("skinning::Mod"), Class));
-    //         mod.init(new ModContext(info, allMods, _ctx.modsLocation));
-    //         mod.enable();
-
-    //     });
-    //     loader.contentLoaderInfo.addEventListener(IOErrorEvent.IO_ERROR,errorHandler,false,0,true);
-    //     final ctx = new LoaderContext();
-    //     ctx.applicationDomain = new ApplicationDomain(ApplicationDomain.currentDomain);
-    //     ctx.allowCodeImport = true;
-    //     final uri = Path.join([_ctx.modsLocation, info.file]);
-    //     trace("about to load "+uri);
-    //     loader.load(new URLRequest(uri), ctx);
-    // }
 
     static function errorHandler(param1:IOErrorEvent)  {
         trace("error: "+param1);
--- mods/example/skinning/Mod.hx
+++ mods/example/skinning/Mod.hx
@@ -1,11 +1,8 @@
 package skinning;
 
-import fl.controls.CheckBox;
-import flash.Lib;
 import blocks.Blocks;
 import flash.geom.Point;
 import flash.display.BitmapData;
-import api.Game;
 
 @:bitmap("skinning/bb2.png") class DarkBasic2 extends BitmapData {}
 
@@ -14,11 +11,5 @@ final class Mod extends modding.ModBase {
         trace("DARKSKIN");
         final dark = new DarkBasic2(30,30);
         Blocks.basic2Bitmap.copyPixels(dark, dark.rect, new Point(0,0));
-
-        // Lib.current.stage.addChild(new CheckBox());
-        Game.getStage().addChild(new CheckBox());
-    }
-
-    public override function disable() {
     }
 }
