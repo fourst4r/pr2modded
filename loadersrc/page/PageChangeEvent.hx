495ada8f2cc6d15627925cded4d62fac8fc786cc
fourst4r <oxygenmagnet@hotmail.com>
2024-06-23 23:52:35 +1200

    Update PageChangeEvent.hx

--- loadersrc/page/PageChangeEvent.hx
+++ loadersrc/page/PageChangeEvent.hx
@@ -3,11 +3,13 @@ package page;
 import flash.events.Event;
 
 final class PageChangeEvent extends flash.events.Event {
+    public static inline final PAGE_CHANGE = "pageChange";
+
     public final from:Page;
     public final to:Page;
 
     public function new(from:Page, to:Page) {
-        super(Event.CHANGE, false, true);
+        super(PAGE_CHANGE, false, true);
         this.from = from;
         this.to = to;
     }
